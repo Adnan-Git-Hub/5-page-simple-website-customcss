93ee66fb495f895454de6ea4e7802fa824e307e6
Adnan-Git-Hub <adnanmasih8@gmail.com>
2024-12-04 02:04:33 +0500

    new

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,6 +1,5 @@
 import '../styles/apply.css';
 export default function Apply() {
-    throw new Error(); // Keeps the error functionality intact
     return (
       <div>
         <h1 className="apply-title">Apply Page!</h1>
